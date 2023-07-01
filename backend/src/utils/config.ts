045b5ec6b5b2b805a8e6c98e6714738c5fe26d10
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-07-01 12:44:57 +0200

    [ADD] generate spotify token utils

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -5,9 +5,13 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL || "";
 const HASH_SECRET = process.env.HASH_SECRET || "dumb-secret";
+const CLIENT_ID = process.env.CLIENT_ID || "";
+const CLIENT_SECRET = process.env.CLIENT_SECRET || "";
 
 export default {
   PORT,
   DB_URL,
   HASH_SECRET,
+  CLIENT_ID,
+  CLIENT_SECRET,
 };
---
+++ backend/src/utils/spotify.ts
@@ -0,0 +1,38 @@
+import config from "./config";
+
+const TOKEN_URL = "https://accounts.spotify.com/api/token";
+
+const auth = Buffer.from(
+  `${config.CLIENT_ID}:${config.CLIENT_SECRET}`
+).toString("base64");
+
+let access_token: string | undefined;
+let expires_in = 3480; // 1h - 2m
+
+export const getSpotifyToken = async () => {
+  if (!access_token) {
+    const { access_token: token } = await fetchSpotifyToken();
+    access_token = token;
+
+    setInterval(fetchSpotifyToken, expires_in * 1000);
+  }
+  return access_token;
+};
+
+const fetchSpotifyToken = async () => {
+  console.log("Fetching Spotify token...");
+
+  const res = await fetch(TOKEN_URL, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ grant_type: "client_credentials" }),
+  });
+
+  const { access_token } = await res.json();
+
+  console.log("Spotify token fetched!");
+  return { access_token };
+};
