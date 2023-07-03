48997c864ec838001d42ede0611851abb826b43d
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-07-03 13:11:56 +0200

    [ADD] playlist model

---
+++ backend/src/db/Playlists.ts
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+import regex from "../utils/regex";
+
+export interface IPlaylist {
+  title: string;
+  description: string;
+  tags: string[];
+  isPrivate: boolean;
+  tracks: string[];
+  creator: mongoose.Schema.Types.ObjectId;
+  followers: mongoose.Schema.Types.ObjectId[];
+}
+
+const PlaylistSchema = new mongoose.Schema<IPlaylist>({
+  title: {
+    type: String,
+    required: true,
+    match: [
+      regex.playlistTitle,
+      `Please insert a valid title: ${regex.playlistTitleDescr}`,
+    ],
+  },
+  description: {
+    type: String,
+    required: true,
+    match: [
+      regex.playlistDescription,
+      `Please insert a valid description: ${regex.playlistDescriptionDescr}`,
+    ],
+  },
+  tags: {
+    type: [
+      {
+        type: String,
+        match: [
+          regex.playlistTag,
+          `Please insert a valid tag: ${regex.playlistTagDescr}`,
+        ],
+      },
+    ],
+    default: [],
+  },
+  isPrivate: {
+    type: Boolean,
+    default: false,
+  },
+  tracks: {
+    type: [String],
+    default: [],
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  followers: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+  },
+});
+
+export const PlaylistModel = mongoose.model("Playlist", PlaylistSchema);
+
+export const getPlaylists = () => PlaylistModel.find();
+export const getPlaylistsByCreator = (
+  creatorId: mongoose.Schema.Types.ObjectId
+) => PlaylistModel.find({ creator: creatorId });
+export const getPlaylistById = (id: string) => PlaylistModel.findById(id);
+export const createPlaylist = (values: Record<string, any>) => {
+  new PlaylistModel(values).save().then((playlist) => playlist.toObject());
+};
+export const deletePlaylistById = (id: string) =>
+  PlaylistModel.findByIdAndDelete(id);
+export const updatePlaylistById = (id: string, values: Record<string, any>) =>
+  PlaylistModel.findByIdAndUpdate(id, values);
--- backend/src/utils/regex.ts
+++ backend/src/utils/regex.ts
@@ -5,13 +5,24 @@ const username = /^[\w\-]{4,20}$/;
 const usernameDescr =
   "4-20 characters, letters, numbers, hyphens and underscores";
 
-const name = /^[\w\ ]{4,25}$/;
+const name = /^[\w\s]{4,25}$/;
 const nameDescr = "4-25 characters, letters, numbers, spaces";
 
 const password =
   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&^])[A-Za-z\d@$!%*#?&^]{8,40}$/;
 const passwordDescr =
   "8-40 characters, at least one uppercase, one lowercase, one number and one special character (e.g. @$!%*#?&^)";
+
+const playlistTitle = /^[\w\s\-.,!?:]{4,50}$/;
+const playlistTitleDescr =
+  "4-50 characters, letters, numbers, spaces and special characters (e.g. -.,!?:)";
+
+const playlistDescription = /^[\w\s\-.,!?:\n@$!%*#?&^]{4,200}$/;
+const playlistDescriptionDescr =
+  "4-200 characters, letters, numbers, special characters (e.g. -.,!?:@$!%*#?&^), new line";
+
+const playlistTag = /[\w\-]{2,16}$/;
+const playlistTagDescr = "2-16 characters, letters, numbers, hyphens";
 
 export default {
   email,
@@ -22,4 +33,10 @@ export default {
   nameDescr,
   password,
   passwordDescr,
+  playlistTitle,
+  playlistTitleDescr,
+  playlistDescription,
+  playlistDescriptionDescr,
+  playlistTag,
+  playlistTagDescr,
 };
