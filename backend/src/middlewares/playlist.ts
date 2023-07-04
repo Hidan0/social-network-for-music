b4ebba61f2e26c0998413d85893367c5f10fb0f6
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-07-04 22:35:20 +0200

    [WIP] midlleware for add/rem tracks from playlis

--- backend/src/middlewares/playlist.ts
+++ backend/src/middlewares/playlist.ts
@@ -28,3 +28,36 @@ export const isPlaylistAuthor = async (
     return res.status(500).json({ message: error.message });
   }
 };
+
+export const isPlaylistAuthorOrCollaborator = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { playlistId } = req.params;
+    const userId = req.identity._id;
+
+    const playlist = await getPlaylistById(playlistId);
+    if (!playlist) {
+      return res.status(404).json({ message: "Playlist not found" });
+    }
+
+    if (
+      playlist.author.toString() !== userId.toString() ||
+      !playlist.collaborators.includes(userId.toString())
+    ) {
+      return res
+        .status(403)
+        .json({
+          message: "You are not the author or collaborator of the playlist",
+        });
+    }
+
+    req.playlist = playlist;
+    next();
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
