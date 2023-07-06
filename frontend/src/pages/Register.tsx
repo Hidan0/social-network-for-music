77d5606df1bec6ed9c494ff645141e2fe16dc690
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-07-07 00:34:53 +0200

    [WIP] register form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,10 +1,127 @@
 import { Component } from "solid-js";
-import { Container } from "solid-bootstrap";
+import { Container, Button } from "solid-bootstrap";
+import { z } from "zod";
+import { createForm, zodForm, SubmitHandler } from "@modular-forms/solid";
+import { InputControl } from "../components/auth/InputControl";
+
+const registerSchema = z
+  .object({
+    email: z
+      .string()
+      .email({ message: "Invalid email address: local-name@domain" }),
+    username: z
+      .string()
+      .min(4, { message: "Username must be at least 4 characters long" })
+      .max(20, { message: "Username must be less than 20 characters long" })
+      .regex(/^[\w\-]{4,20}$/, {
+        message:
+          "Username must only contain alphanumeric characters, underscores, and hyphens",
+      }),
+    name: z
+      .string()
+      .min(4, { message: "Name must be at least 4 characters long" })
+      .max(25, { message: "Name must be less than 25 characters long" })
+      .regex(/^[\w\s]{4,25}$/, {
+        message: "Name must only contain alphanumeric characters and spaces",
+      }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .max(40, { message: "Password must be less than 40 characters long" })
+      .regex(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&^])[A-Za-z\d@$!%*#?&^]{8,40}$/,
+        {
+          message:
+            "Password must contain at least one letter, number, and special character",
+        }
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
+
+type RegisterForm = z.infer<typeof registerSchema>;
 
 const RegisterScreen: Component = () => {
+  const [, { Form: ZForm, Field }] = createForm<RegisterForm>({
+    validate: zodForm(registerSchema),
+  });
+
+  const handleSubmit: SubmitHandler<RegisterForm> = (values, event) => {
+    // Runs on client
+    console.log("SUBMIT");
+    console.log(values);
+  };
+
   return (
     <Container>
-      <h1>Hello world</h1>
+      <ZForm onSubmit={handleSubmit} class="needs-validation">
+        <Field name="email">
+          {(field, props) => (
+            <InputControl
+              {...props}
+              type="email"
+              label="Email Address"
+              value={field.value}
+              error={field.error}
+              required
+            />
+          )}
+        </Field>
+        <Field name="username">
+          {(field, props) => (
+            <InputControl
+              {...props}
+              type="text"
+              label="Username"
+              value={field.value}
+              error={field.error}
+              required
+            />
+          )}
+        </Field>
+        <Field name="name">
+          {(field, props) => (
+            <InputControl
+              {...props}
+              type="text"
+              label="Name"
+              value={field.value}
+              error={field.error}
+              required
+            />
+          )}
+        </Field>
+        <Field name="password">
+          {(field, props) => (
+            <InputControl
+              {...props}
+              type="password"
+              label="Password"
+              value={field.value}
+              error={field.error}
+              required
+            />
+          )}
+        </Field>
+        <Field name="confirmPassword">
+          {(field, props) => (
+            <InputControl
+              {...props}
+              type="password"
+              label="Confirm Password"
+              value={field.value}
+              error={field.error}
+              required
+            />
+          )}
+        </Field>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </ZForm>
     </Container>
   );
 };
