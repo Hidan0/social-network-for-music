f0ce309c3e6f21b8ef684786c46a798ec39af92e
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-07-09 15:15:21 +0200

    [MOD] post -> get

--- backend/src/router/authentication.ts
+++ backend/src/router/authentication.ts
@@ -4,5 +4,5 @@ import { login, register, validToken } from "../controllers/auth";
 export default (router: express.Router) => {
   router.post("/auth/register", register);
   router.post("/auth/login", login);
-  router.post("/auth/verify", validToken);
+  router.get("/auth/verify", validToken);
 };
