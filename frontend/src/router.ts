7af126dc0558407364fd08388b2c1d57c649f48b
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-07-17 20:47:24 +0200

    [ADD] auth hooks that makes sense

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,31 +1,68 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  RouteLocationNormalized,
+  RouteRecordRaw,
+  createRouter,
+  createWebHistory,
+} from "vue-router";
 
-// import config from "./config";
+import useUserStore from "./stores/user";
+
+export type PageOptions = RouteRecordRaw & {
+  meta?: {
+    requiresAuth?: boolean;
+  };
+};
+
+const pages: PageOptions[] = [
+  {
+    name: "home",
+    path: "/",
+    component: () => import("./pages/HomePage.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    name: "register",
+    path: "/register",
+    component: () => import("./pages/RegisterPage.vue"),
+  },
+  {
+    name: "login",
+    path: "/login",
+    component: () => import("./pages/LoginPage.vue"),
+  },
+  {
+    name: "playlist",
+    path: "/playlist/:id",
+    component: () => import("./pages/PlaylistDashboard.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      name: "home",
-      path: "/",
-      component: () => import("./pages/HomePage.vue"),
-    },
-    {
-      name: "register",
-      path: "/register",
-      component: () => import("./pages/RegisterPage.vue"),
-    },
-    {
-      name: "login",
-      path: "/login",
-      component: () => import("./pages/LoginPage.vue"),
-    },
-    {
-      name: "playlist",
-      path: "/playlist/:id",
-      component: () => import("./pages/PlaylistDashboard.vue"),
-    },
-  ],
+  routes: pages,
+});
+
+const getByRoute = (
+  route: RouteLocationNormalized
+): PageOptions | undefined => {
+  return pages.find((page) => page.path === route.path);
+};
+
+router.beforeEach(async (to, _from) => {
+  const nextPage = getByRoute(to);
+
+  if (nextPage?.meta?.requiresAuth) {
+    const $user = useUserStore();
+    const isValid = await $user.verify();
+    if (!isValid) {
+      router.push({ name: "login" });
+    }
+  }
 });
 
 export default router;
